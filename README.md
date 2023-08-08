66f93f01c52a6eb3981a406a5ebf201e5e89184d
Derek Adair <derek-adair@users.noreply.github.com>
2023-08-08 07:17:28 -0600

    - Scoped out api/ui folders for planned changes - configurable domains via yaml file - (more) proper error handling - docker-compose file

--- README.md
+++ README.md
@@ -5,19 +5,10 @@ This is a BAREBONES email proxy that leverages mailgun.  The goal here is to mak
 ## Usage
 1. Build the docker image
 ```
-docker build -t="mgproxy" .
+docker-compose build
 ```
+2. Create a .env and add MG_KEY=`yourkey`
 
-2. Run the image...
-```
-docker run --env MG_TO={YOUR_EMAIL} --env MG_DOMAIN=YOUR_DOMAIN --env MG_KEY=YOUR_PRIVATE_MAILGUN_KEY mgproxy
-```
+3. Copy/adapt `proxy.config.example.yml` to `proxy.config.yml`
 
-## Features
-The only real magic here is you can easily send emails from subdomains like so... `MG_DOMAIN
-
-## TODO...
-* Release via pypi
-* dev/prod images (gunincorn not flask dev server)
-* add a simple config file to associate domains with specific emails to send to 
-* MAYBE add a whitelist of emails to attempt to send to.  This would allow  configuring MG_TO for each form in the client.
+4. `docker-compose up`
---
+++ api/app.py
@@ -0,0 +1,68 @@
+import os
+from flask import Flask, request, abort
+from flask_cors import CORS
+import requests as req
+from requests.auth import HTTPBasicAuth
+import logging
+
+logging.getLogger('flask_cors').level = logging.DEBUG
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
+from pathlib import Path
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent
+MG_KEY = os.getenv('MG_KEY', "REPLACE_ME_YOUR_KEY")
+
+import yaml
+
+with open(BASE_DIR / "proxy.config.yml", "r") as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+origins = []
+for host in config['domains'].keys():
+    origins += config['domains'][host]['uris']
+
+def get_config_key(request):
+    for host in config['domains'].keys():
+        if request.origin in config['domains'][host]['uris']:
+            return host
+
+    # origin did not match any config
+    return False
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": origins}})
+@app.route('/', methods=['POST'])
+def email():
+    # Automatically read MG_DOMAIN
+    mg_domain = get_config_key(request)
+    domain_config = config['domains'][mg_domain]
+
+    if request.origin not in domain_config['uris']:
+        return abort(400)
+
+    endpoint = 'https://api.mailgun.net/v3/{}/messages'.format(mg_domain)
+    email = request.get_json()
+    email['to'] = domain_config['to']
+
+    app.logger.info("sending email to {}".format(email['to']))
+    app.logger.debug("Endpoint: {}".format(endpoint))
+
+    response = req.post(endpoint, auth=('api', MG_KEY), data=email)
+
+    if response.ok:
+        return response.text
+    else:
+        return abort(response.status_code)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
